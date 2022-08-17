23014d4287f38367e3fe5b3f9eac19fae04dc6d4
RoCarmona <rocarmona2010@hotmail.com>
2022-08-17 16:19:46 -0300

    adicionando exemplos

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemplo2.js
@@ -0,0 +1,10 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// .push() adiciona novo item no final
+// .unshift() adiciona novo item no inicio
+// .pop() remove a última tarefa
