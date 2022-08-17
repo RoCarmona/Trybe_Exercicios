a1bb2ebb530c819cf778aa64519be15d078de5e6
RoCarmona <rocarmona2010@hotmail.com>
2022-08-17 16:03:35 -0300

    Adicionando exercicios

---
+++ Exercises/4.1/script.js
@@ -0,0 +1,11 @@
+const notaPessoaCandidata = 60;
+
+if (notaPessoaCandidata >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)!!");
+} 
+else if (notaPessoaCandidata < 80 || notaPessoaCandidata >= 60) {
+    console.log ("Você está em nossa lista de espera");
+}
+else{
+    console.log ("Você foi reprovada(o)")
+};
