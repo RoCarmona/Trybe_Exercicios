61895596812bd17559977827d9bf6d43e7c20559
RoCarmona <rocarmona2010@hotmail.com>
2022-08-17 21:01:03 -0300

    Exercxios Array

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio1.js
@@ -0,0 +1,12 @@
+// Obtenha o valor "Serviços" do array menu:
+// Procure o índice do valor "Portfólio" do array menu:
+// Adicione o valor "Contato" no final do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu [1];
+let indexOfPortifolio = menu[2]
+menu.push('Contato')
+
+console.log(menuServices);
+console.log(indexOfPortifolio);
+console.log(menu)
