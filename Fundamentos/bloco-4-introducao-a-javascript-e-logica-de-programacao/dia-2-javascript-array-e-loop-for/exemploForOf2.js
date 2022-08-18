8db57548da5962049a496dce04aa45703971adda
RoCarmona <rocarmona2010@hotmail.com>
2022-08-17 21:18:56 -0300

    mais exemplos

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemploForOf2.js
@@ -0,0 +1,12 @@
+// for/of, nós conseguimos somar um valor a cada elemento do array e retorná-los da seguinte forma:
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
