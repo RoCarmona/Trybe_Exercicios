c7aca9f0a85601d507e3de5bf50b7b7e4093f844
RoCarmona <rocarmona2010@hotmail.com>
2022-08-22 18:27:37 -0300

    correção

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio1Array.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio1Array.js
@@ -2,11 +2,18 @@
 // Procure o índice do valor "Portfólio" do array menu:
 // Adicione o valor "Contato" no final do array menu:
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu [1];
-let indexOfPortifolio = menu[2]
-menu.push('Contato')
 
-console.log(menuServices);
-console.log(indexOfPortifolio);
-console.log(menu)
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu [1];
+// let indexOfPortifolio = menu[2]
+// menu.push('Contato')
+
+// console.log(menuServices);
+// console.log(indexOfPortifolio);
+// console.log(menu)
+
+
+for (index = 0; index <= 100; index += 1) {
+  
+};
+console.log(index)
