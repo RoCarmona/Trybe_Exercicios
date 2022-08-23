4b856f13b689e01800131457c40561d38b56d7f2
RoCarmona <rocarmona2010@hotmail.com>
2022-08-23 19:11:18 -0300

    Fixar objetos

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex1-Fixar.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex1-Fixar.js
@@ -17,3 +17,16 @@ let player = {
 // 2 - Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
 console.log('A jogadora'+ player.name + player.lastName + 'tem' + player.age + 'anos de idade');
+
+// 4 - 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+// [2006, 2007, 2008, 2009, 2010, 2018]
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+// 5 - 5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro ' + 'e ' + player.medals.silver + ' medalhhas de prata' )
+
+
