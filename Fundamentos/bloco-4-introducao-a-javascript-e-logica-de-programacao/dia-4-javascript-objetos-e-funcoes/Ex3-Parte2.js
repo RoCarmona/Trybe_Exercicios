fc3bfcd5d7afcdae4e6d04f2048db8b9a180e1b7
RoCarmona <rocarmona2010@hotmail.com>
2022-09-01 16:06:11 -0300

    Parte 2 - Exercico 7 conforme gabarito

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex3-Parte2.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex3-Parte2.js
@@ -138,8 +138,6 @@ console.log(somaNumeros(5));
 
 
 
-
-
 // 7 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
 // Valor de teste: 'trybe' e 'be'
 // Valor esperado no retorno da função: true
@@ -147,4 +145,24 @@ console.log(somaNumeros(5));
 // Retorno esperado: true
 // verificaFimPalavra('joaofernando', 'fernan');
 // Retorno esperado: false
+
+function verificaFimPalavra(palavra, fimPalavra) {
+    let inversoPalavra = palavra.split('').reverse().join('');
+    let inversoFimPalavra = fimPalavra.split('').reverse().join('');
+    let result;
+  
+    for (let index = 0; index < inversoFimPalavra.length; index += 1) {
+      if (inversoPalavra[index] !== inversoFimPalavra[index]) {
+        result = false;
+        break; // O "break" serve para encerrar o loop.
+      } else {
+        result = true;
+      }
+    }
+    
+    return result;
+  }
+  
+  console.log(verificaFimPalavra('trybe', 'be')); //true
+  console.log(verificaFimPalavra('joaofernando', 'fernan')); //false
 
