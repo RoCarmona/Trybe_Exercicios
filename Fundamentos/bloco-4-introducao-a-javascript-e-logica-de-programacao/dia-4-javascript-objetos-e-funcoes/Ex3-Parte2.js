e7329e08f1b4079b6c2e0b6ade2a1885239cfad8
RoCarmona <rocarmona2010@hotmail.com>
2022-09-01 15:40:57 -0300

    Exercicio 5 com as explicações do gabarito

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex3-Parte2.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Ex3-Parte2.js
@@ -93,6 +93,34 @@ console.log(nomeMaiorCaracteres(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo'
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2.
 
+function maisRepetido(numeros) {
+    let num = {};
+  
+    for (let index = 0; index < numeros.length; index += 1) {
+      let valor = numeros[index];
+      if (num[valor] === undefined) {
+        num[valor] = 1;
+      } else {
+        num[valor] = num[valor] + 1;
+      }
+    }
+  
+    let contRepetido = 0;
+    let contNumero = 0;
+  
+    for (let prop in num) {
+      if (contRepetido < num[prop]) {
+        contRepetido = num[prop];
+        contNumero = prop;
+      }
+    }
+  
+    return contNumero;
+  }
+  
+  console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); // 2
+
+
 
 // 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
 // Valor de teste: N = 5.
